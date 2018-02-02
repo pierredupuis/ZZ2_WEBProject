f89b0b9749e27507ea31822245d21099241a22c4
Yassir Karroum <ukarroum17@gmail.com>
2018-02-02 15:08:32 +0100

    Fix gitignore add Controllers and DTOs

---
+++ DAL & API/ApiGOT/Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ApiGOT.Models;
+using DataAccessLayer;
+
+namespace ApiGOT.Controllers
+{
+    public class CharacterController : ApiController
+    {
+        public List<CharacterDTO> getAllCharacters()
+        {
+            List<CharacterDTO> list = new List<CharacterDTO>();
+
+            DalManager m = DalManager.Instance;
+
+            foreach(var charac in m.getAllCharacters())
+            {
+                list.Add(new CharacterDTO(charac));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ DAL & API/ApiGOT/Models/CharacterDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+using EntitiesLayer;
+
+namespace ApiGOT.Models
+{
+    public class CharacterDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bravoury { get; set; }
+        public string Crazyness { get; set; }
+        public string Pv { get; set; }
+
+        public CharacterDTO(Character character)
+        {
+            Id = character.Id;
+            FirstName = character.FirstName;
+            LastName = character.LastName;
+            Bravoury = character.Bravoury;
+            Crazyness = character.Crazyness;
+            Pv = Pv;
+        }
+
+    }
+}
