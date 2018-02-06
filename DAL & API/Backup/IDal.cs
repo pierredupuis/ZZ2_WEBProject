0cbe58aa3a32fb806b87af53e49f478284aefde1
MADDXYZ <enneiymy.i@gmail.com>
2018-02-06 15:40:07 +0100

    Fixed the framework type

---
+++ DAL & API/Backup/IDal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntitiesLayer;
+
+namespace DataAccessLayer
+{
+    interface IDal
+    {
+        List<House> GetHouses();
+        House GetHouseById(int h);
+        /*List<Fight> GetFights();
+        Fight GetFightById(int f);
+        List<Territory> GetTerritories();
+        Territory GetTerritoryById(int t);
+        List<Character> GetCharacters();
+        Character GetCharacterById(int c);
+        void AddHouse(House h);
+        void AddCharacter(Character c);
+        void AddTerritory(Territory t);
+        void AddFight(Fight f);
+        void DeleteHouse(House h);
+        void DeleteCharacter(Character c);
+        void DeleteTerritory(Territory t);
+        void DeleteFight(Fight f);
+        void UpdateHouse(House h);
+        void UpdateCharacter(Character c);
+        void UpdateTerritory(Territory t);
+        void UpdateFight(Fight f);*/
+    }
+}
---
+++ DAL & API/EntitiesLayer_2/EntityObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntitiesLayer
+{
+    public class EntityObject
+    {
+        protected int id;
+        public enum Character_t { WARRIOR, WITCH, TACTICIAN, LEADER, LOSER };
+        public enum Relathionships_t { FRIENDSHIP, LOVE, HATRED, RIVALRY };
+        public enum Territory_t { SEA, MOUNTAIN, LAND, DESERT };
+    }
+}
